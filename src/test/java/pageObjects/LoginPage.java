1ebe5877dcf4c846a6a189e8492ea665b857c400
nehajain221292 <neha_jain@epam.com>
2025-09-04 12:38:22 +0530

    Create src/test/java/pageObjects/LoginPage.java

---
+++ src/test/java/pageObjects/LoginPage.java
@@ -0,0 +1,29 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By emailField = By.name("email");
+    By passwordField = By.name("password");
+    By loginButton = By.xpath("//button[text()='Login']");
+
+    public void enterEmail(String email) {
+        driver.findElement(emailField).sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
+    }
+}
