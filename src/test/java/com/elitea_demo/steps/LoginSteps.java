84e82e82dd1667faf67274092ad8a64c2c691d9e
neha_jain <neha_jain@epam.com>
2025-09-04 15:58:10 +0530

    Update LoginSteps to use WebDriverManager and update dependencies

--- src/test/java/com/elitea_demo/steps/LoginSteps.java
+++ src/test/java/com/elitea_demo/steps/LoginSteps.java
@@ -4,10 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.And;
+
+import java.time.Duration;
+
 import org.junit.Assert;
 
 public class LoginSteps {
@@ -15,7 +21,7 @@ public class LoginSteps {
 
     @Given("I navigate to {string}")
     public void i_navigate_to(String url) {
-        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get(url);
     }
@@ -30,13 +36,15 @@ public class LoginSteps {
 
     @And("I click on the {string} button")
     public void i_click_on_the_button(String button) {
-        WebElement loginButton = driver.findElement(By.xpath("//button[text()='" + button + "']"));
+        WebElement loginButton = driver.findElement(By.xpath("//button/span[text()='" + button + "']"));
         loginButton.click();
     }
 
     @Then("I should see the dashboard")
     public void i_should_see_the_dashboard() {
-        WebElement dashboard = driver.findElement(By.id("dashboard"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(d -> d.findElement(By.xpath("//span[text()='Dashboard']")));
+        WebElement dashboard = driver.findElement(By.xpath("//span[text()='Dashboard']"));
         Assert.assertTrue(dashboard.isDisplayed());
         driver.quit();
     }
