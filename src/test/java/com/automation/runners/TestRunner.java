0877ea94486bea28ff5675aec880255147a6f3c1
nehajain221292 <neha_jain@epam.com>
2025-09-11 13:35:18 +0530

    Create src/test/java/com/automation/runners/TestRunner.java

---
+++ src/test/java/com/automation/runners/TestRunner.java
@@ -0,0 +1,13 @@
+package com.automation.runners;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/com/automation/tests",
+        glue = "com.automation.steps"
+)
+public class TestRunner {
+}
