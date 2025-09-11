d2cf5226f1cb09e57df09db1b90920ac9e07568d
nehajain221292 <neha_jain@epam.com>
2025-09-11 15:06:26 +0530

    Create src/test/java/steps/LoginSteps.java

---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,46 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.assertTrue;
+
+public class LoginSteps {
+    WebDriver driver;
+
+    @Given("I navigate to {string}")
+    public void i_navigate_to(String url) {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.get(url);
+    }
+
+    @When("I enter email {string}")
+    public void i_enter_email(String email) {
+        WebElement emailField = driver.findElement(By.name("email"));
+        emailField.sendKeys(email);
+    }
+
+    @When("I enter password {string}")
+    public void i_enter_password(String password) {
+        WebElement passwordField = driver.findElement(By.name("password"));
+        passwordField.sendKeys(password);
+    }
+
+    @When("I click on the {string} button")
+    public void i_click_on_the_button(String button) {
+        WebElement loginButton = driver.findElement(By.xpath("//button[contains(text(), 'Login')]"));
+        loginButton.click();
+    }
+
+    @Then("I should see the dashboard")
+    public void i_should_see_the_dashboard() {
+        WebElement dashboard = driver.findElement(By.id("dashboard"));
+        assertTrue(dashboard.isDisplayed());
+        driver.quit();
+    }
+}
