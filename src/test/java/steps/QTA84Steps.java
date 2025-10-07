037692c2a0f5fe9c543a349fc940c3b91ff778a8
nehajain221292 <njain0283@gmail.com>
2025-10-07 16:12:32 +0530

    Create src/test/java/steps/QTA84Steps.java

---
+++ src/test/java/steps/QTA84Steps.java
@@ -0,0 +1,48 @@
+package steps;
+
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.Assert;
+
+public class QTA84Steps {
+    WebDriver driver;
+
+    @Given("I am logged in")
+    public void i_am_logged_in() {
+        LoginSteps loginSteps = new LoginSteps();
+        loginSteps.navigateToLoginPage();
+        loginSteps.enterValidCredentials();
+        loginSteps.clickLoginButton();
+        loginSteps.verifyDashboardIsVisible();
+        this.driver = loginSteps.driver;
+    }
+
+    @When("I navigate to the visa section")
+    public void i_navigate_to_the_visa_section() {
+        WebElement visaSection = driver.findElement(By.linkText("Visa"));
+        visaSection.click();
+    }
+
+    @When("I fill from country as {string} and to country as {string}")
+    public void i_fill_from_country_as_and_to_country_as(String fromCountry, String toCountry) {
+        WebElement fromCountryField = driver.findElement(By.name("from_country"));
+        WebElement toCountryField = driver.findElement(By.name("to_country"));
+        fromCountryField.sendKeys(fromCountry);
+        toCountryField.sendKeys(toCountry);
+    }
+
+    @When("I click on the search button")
+    public void i_click_on_the_search_button() {
+        WebElement searchButton = driver.findElement(By.xpath("//button[contains(text(), 'Search')]"));
+        searchButton.click();
+    }
+
+    @Then("I should see the {string} message")
+    public void i_should_see_the_message(String errorMessage) {
+        Assert.assertTrue(driver.getPageSource().contains(errorMessage));
+        driver.quit();
+    }
+}
